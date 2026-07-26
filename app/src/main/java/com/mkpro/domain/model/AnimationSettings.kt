a1e6add9aa18063515263f9793f95bb1ee90ccb8
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-26 16:22:23 +0300

    Delete AnimationSettings.kt

--- app/src/main/java/com/mkpro/domain/model/AnimationSettings.kt
+++
@@ -1,8 +0,0 @@
-package com.mkpro.domain.model
-
-data class AnimationSettings(
-    val pressDurationMs: Long = 80L,
-    val rippleDurationMs: Long = 300L,
-    val glowEnabled: Boolean = true,
-    val shadowAnimationEnabled: Boolean = true
-)
