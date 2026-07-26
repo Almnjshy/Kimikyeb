33e12318d05e6d199227b87cdf8acf1453472563
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-26 16:14:58 +0300

    Create ModifierKey.kt

---
+++ app/src/main/java/com/mkpro/domain/model/ModifierKey.kt
@@ -0,0 +1,12 @@
+package com.mkpro.domain.model
+
+/**
+* Enum class representing modifier keys on a keyboard
+*/
+enum class ModifierKey {
+   SHIFT,
+   CTRL,
+   ALT,
+   META,  // Windows key / Command key
+   FN     // Function key
+}
