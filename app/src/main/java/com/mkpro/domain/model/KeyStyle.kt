c856ab631b19410c9beb3ff28f9a9931bd89875a
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-26 16:12:28 +0300

    Create KeyStyle.kt

---
+++ app/src/main/java/com/mkpro/domain/model/KeyStyle.kt
@@ -0,0 +1,12 @@
+package com.mkpro.domain.model
+
+data class KeyStyle(
+    val cornerRadius: Float,
+    val elevation: Float,
+    val elevationPressed: Float,
+    val borderWidth: Float,
+    val fontSize: Float,
+    val iconSize: Float,
+    val keySpacing: Float,
+    val rowSpacing: Float
+)
